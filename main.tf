9fa8f0b206ba5c863636b9c3a3e60660109862d3
Alex_Chavez <chavezzz@gmail.com>
2021-01-04 16:00:59 -0600

    adds ssh keys.

--- main.tf
+++ main.tf
@@ -13,4 +13,10 @@ resource "digitalocean_droplet" "web" {
   name   = "web-1"
   region = "NYC1"
   size   = "s-1vcpu-1gb"
+  ssh_keys = [digitalocean_ssh_key.default.fingerprint]  
+}
+
+resource "digitalocean_ssh_key" "default" {
+  name       = "Droplet access."
+  public_key = file("~/.ssh/digital_ocean.pem.pub")
 }
